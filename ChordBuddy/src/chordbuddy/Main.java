b31e4b6bcf38a17ba483ae06f9aecc4eddc892dc
Ryan DeFord <Ryan@ryan-defords-powerbook-g4-15.local>
2013-11-09 23:46:56 -0500

    creating initial netbeans project

---
+++ ChordBuddy/src/chordbuddy/Main.java
@@ -0,0 +1,15 @@
+package chordbuddy;
+
+/**
+ *
+ * @author Ryan
+ */
+public class Main {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello, ChordBuddy!");
+  }
+}
